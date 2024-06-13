f249db8e6939e1c1cee695c85843ec3e13cf7551
SatyaSundarSaikia <satyasundarsaikia@gmail.com>
2024-06-13 17:46:07 +0530

    first commit

---
+++ web_map/main.js
@@ -0,0 +1,31 @@
+window.onload = init
+
+function init() {
+    const map = new ol.Map({
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2,
+            
+        }),
+            layers: [
+            new ol.layer.Tile({
+            source: new ol.source.OSM() 
+        })  
+       ],  
+            target: "Js-map"
+            })
+            const popupContainerElement = document.getElementById('popup-coordinates');
+
+            const popup = new ol.Overlay({
+                element: popupContainerElement
+            })
+            map.addOverlay(popup);
+
+            map.on ('click' , function(e){
+               const clickedCoordinate = e.coordinate;
+               popup.setPosition(undefined);
+               popup.setPosition(clickedCoordinate);
+               popupContainerElement.innerHTML = clickedCoordinate;
+            })
+
+        }
